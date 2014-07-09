a62f0cc15c7b67f9d83e40b47d973f112991399a
takasing <takaty2011@gmail.com>
2014-07-09 14:55:55 +0900

    add pre-commit template, and refactor init.sh.

--- init.sh
+++ init.sh
@@ -2,9 +2,32 @@
 
 path="$(cd `dirname $0`;pwd)"
 
-test -f ~/.bash_profile  || ln -s $path/.bash_profile ~/.bash_profile
-test -f ~/.vimrc  || ln -s $path/.vimrc ~/.vimrc
-test -f ~/.tmux.conf  || ln -s $path/.tmux.conf ~/.tmux.conf
+function create_symlink() {
+  if [ ! -e ~/$1 ]; then
+    ln -s $path/$1 ~/$1
+    echo "create symbolic link: $1"
+  fi
+}
+
+# TODO: for
+create_symlink .bash_profile
+create_symlink .vimrc
+create_symlink .tmux.conf
+create_symlink .jshintrc
+create_symlink .git_template
+# TODO: improve
+authorization=`ls -l ~/.git_template/hooks/pre-commit | grep "rwxr-xr-x" | wc -l`
+if [ $authorization -lt 1 ]; then
+  chmod 755 ~/.git_template/hooks/pre-commit
+  echo "make pre-commit executable."
+fi
+init_temp=`cat ~/.gitconfig | grep ".git_template" | wc -l`
+if [ $init_temp -lt 1 ]; then
+  git config --global init.templatedir '~/.git_template'
+  echo "config init template."
+fi
+
+# zsh
 zsh init_zprezto.sh
-test -f ~/.zshrc  || ln -s $path/.zshrc ~/.zshrc
-test -f ~/.jshintrc || ln -s $path/.jshintrc ~/.jshintrc
+create_symlink .zshrc
+
