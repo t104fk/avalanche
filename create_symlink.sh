4da728f42d07dba056476f0443d26f72cb84e9dd
takasing <takaty2011@gmail.com>
2014-08-07 19:19:32 +0900

    add ssh-copy-id.

--- create_symlink.sh
+++ create_symlink.sh
@@ -4,7 +4,7 @@ path="$(cd `dirname $0`;pwd)"
 
 function create_symlink() {
   if [ ! -e ~/$1 ]; then
-    ln -s $path/$1 ~/$1
+    ln -s $path/$1 $2
     echo "create symbolic link: $1"
   fi
 }
@@ -12,6 +12,8 @@ function create_symlink() {
 DOTFILES=(.vimrc .tmux.conf .jshintrc .git_template .vim.local .bin)
 for file in ${DOTFILES[@]}
 do
-  create_symlink $file
+  create_symlink $file ~/$file
 done
+
+create_symlink utils/ssh-copy-id /usr/local/bin/ssh-copy-id
 
