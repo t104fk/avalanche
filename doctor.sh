e6d9c9795e97ad4e392e06c2bc23fb7effe08ccd
SET_ME_LOCALLY <SET_ME_LOCALLY>
2014-08-21 16:16:36 +0000

    add doctor.

---
+++ doctor.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+UNAME=`uname -a | awk '{print $1}'`
+case $UNAME in
+  Linux)
+    echo "Linux"
+    COMMANDS=(ruby git tmux rbenv zsh)
+    ;;
+  Darwin)
+    echo "MaxOSX"
+    COMMANDS=(ruby brew git tmux nodebrew rbenv zsh)
+    ;;
+esac
+for command in ${COMMANDS[@]}
+do
+  if ! which $command 2>/dev/null; then
+    echo "$command is not."
+  fi
+done
