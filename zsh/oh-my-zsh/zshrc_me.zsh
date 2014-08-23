c5646945f28c9dfa6a73bacc52844dc9f85381f7
takasing <takaty2011@gmail.com>
2014-08-23 22:30:23 +0900

    add google cloud sdk.

--- zsh/oh-my-zsh/zshrc_me.zsh
+++ zsh/oh-my-zsh/zshrc_me.zsh
@@ -20,3 +20,8 @@ if [ $SHLVL = 1 ]; then
   tmux a || tmux -f $HOME/.tmux.conf
 fi
 
+# The next line updates PATH for the Google Cloud SDK.
+source '/usr/local/google-cloud-sdk/path.zsh.inc'
+# The next line enables bash completion for gcloud.
+source '/usr/local/google-cloud-sdk/completion.zsh.inc'
+
