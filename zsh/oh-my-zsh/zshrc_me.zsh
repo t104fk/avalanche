fabf6c8f2634d4a0753e905682153c0b080a23fb
SET_ME_LOCALLY <SET_ME_LOCALLY>
2014-09-03 17:41:31 +0900

    comment out gce sdk.

--- zsh/oh-my-zsh/zshrc_me.zsh
+++ zsh/oh-my-zsh/zshrc_me.zsh
@@ -21,7 +21,7 @@ if [ $SHLVL = 1 ]; then
 fi
 
 # The next line updates PATH for the Google Cloud SDK.
-source '/usr/local/google-cloud-sdk/path.zsh.inc'
+#source '/usr/local/google-cloud-sdk/path.zsh.inc'
 # The next line enables bash completion for gcloud.
-source '/usr/local/google-cloud-sdk/completion.zsh.inc'
+#source '/usr/local/google-cloud-sdk/completion.zsh.inc'
 
