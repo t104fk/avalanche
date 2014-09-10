5184b5d7dd5c43a10a65424ea341f513424313c7
takasig <takaty2011@gmail.com>
2014-09-10 09:41:05 +0900

    add installing tmuxinator, and some package by brew.

--- homebrew/brewfile.sh
+++ homebrew/brewfile.sh
@@ -7,7 +7,7 @@ do
   [ $exists -lt 1 ] && brew tap $t
 done
 
-PACKAGES=(git tmux mysql55 reattach-to-user-namespace csshx nkf brew-cask htop peco ghq wget)
+PACKAGES=(git tmux mysql55 reattach-to-user-namespace csshx nkf brew-cask htop peco ghq wget maven jq)
 for p in ${PACKAGES[@]}
 do
   brew list $p >& /dev/null
