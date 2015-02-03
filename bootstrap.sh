51e738aaa37c9ea465131aa2b08debcdde1d326e
takasig <takaty2011@gmail.com>
2015-02-03 14:31:39 +0900

    make create symlink last.

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,6 +7,6 @@ sh homebrew/install.sh
 sh ruby/install_rbenv.sh
 sh nodebrew/setup_nodebrew.sh
 sh vim/setup_vim.sh
-sh create_symlink.sh
 sh git/setup_git.sh
+sh create_symlink.sh
 echo "relaunch terminal."
