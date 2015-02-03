458959f885ae06ec25178fb3dfcf9e3814e82f46
takasig <takaty2011@gmail.com>
2015-02-03 13:54:56 +0900

    multi platform support.

--- zsh/setup_zsh.sh
+++ zsh/setup_zsh.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-#sh zsh/oh-my-zsh/init_oh-my-zsh.sh
+dir=$(cd $(dirname $0);pwd)
+sh $dir/oh-my-zsh/init_oh-my-zsh.sh
 if [ "/bin/zsh" != $SHELL ]; then
-  case `uname -a | awk '{print $1}'` in
-    Darwin)
-      chsh -s /bin/zsh
-      ;;
-    Linux)
-      # TODO: ubuntu, centos
-      sudo chsh ec2-user -s /bin/zsh
-      ;;
-  esac
+  if [ `uname -a | awk '{print $1}'` = "Darwin" ];then
+    chsh -s /bin/zsh
+  elif [ -f /etc/redhat-release ];then
+    sudo chsh `whoami` -s /bin/zsh
+  elif [ -f /etc/system-release ];then
+    sudo chsh ec2-user -s /bin/zsh
+  else
+    echo "we doesn't support your platform."
+  fi
 fi
 
