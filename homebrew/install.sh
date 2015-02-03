7b3c4a4cf1f85705bbf983c027e87f1689431b8d
takasig <takaty2011@gmail.com>
2015-02-03 14:11:20 +0900

    mac check.

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# TODO: mac only
+if [ `uname -a | awk '{print $1}'` = "Darwin" ];then
+  echo "you don't need to install homebrew."
+  exit 0
+fi
 if ! which brew 2>/dev/null; then
   echo "brew is not."
   ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go/install)"
