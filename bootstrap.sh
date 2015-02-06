671912ff2dd92c4f09210e9b7e83f615e1d4c620
takasig <takaty2011@gmail.com>
2015-02-06 12:49:39 +0900

    install several tools manually.

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,9 +3,6 @@
 # TODO: check ostype, and least environment.
 # if not installed oh-my-zsh, stop bootstrap.
 sh zsh/setup_zsh.sh
-sh homebrew/install.sh
-sh ruby/install_rbenv.sh
-sh nodebrew/setup_nodebrew.sh
 sh vim/setup_vim.sh
 sh git/setup_git.sh
 sh create_symlink.sh
