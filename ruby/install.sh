466f2767000360200202e86741c26730a37f6da9
takasig <takaty2011@gmail.com>
2015-02-06 13:14:23 +0900

    rename and install/update.

---
+++ ruby/install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+current="$(cd `dirname $0`;pwd)"
+source $current/rbenv.sh
+source $current/update_ruby.sh
---
+++ ruby/update.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+current="$(cd `dirname $0`;pwd)"
+source $current/update_default_gems.sh
+source $current/update_ruby.sh
