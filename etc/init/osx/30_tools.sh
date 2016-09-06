2e89353db7d6e894dd1523f4fb6136f394eb7094
takasing <takaty2011@gmail.com>
2016-09-06 11:44:38 +0900

    Resolve [Errno 13] Permission denied: '/Library/Python/2.7/site-packages/test-easy-install-4927.pth'

--- etc/init/osx/30_tools.sh
+++ etc/init/osx/30_tools.sh
@@ -8,10 +8,10 @@ if is_exists "brew" && is_exists "easy_install" ; then
   case "$PLATFORM" in
     osx)
       brew install freetype
-      easy_install blockdiag
-      easy_install seqdiag
-      easy_install actdiag
-      easy_install nwdiag
+      sudo easy_install blockdiag
+      sudo easy_install seqdiag
+      sudo easy_install actdiag
+      sudo easy_install nwdiag
       ;;
     *)
       echo "???"
