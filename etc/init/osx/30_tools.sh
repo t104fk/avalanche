01cf72423c54507d316261fa562f466fc8aeec63
takasing <takaty2011@gmail.com>
2016-09-06 12:03:23 +0900

    install openjpeg

--- etc/init/osx/30_tools.sh
+++ etc/init/osx/30_tools.sh
@@ -7,7 +7,7 @@ source "$DOTPATH"/etc/lib/vital.sh
 if is_exists "brew" && is_exists "easy_install" ; then
   case "$PLATFORM" in
     osx)
-      brew install freetype
+      brew install freetype openjpeg
       sudo easy_install blockdiag
       sudo easy_install seqdiag
       sudo easy_install actdiag
