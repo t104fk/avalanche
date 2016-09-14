e0c876f8dbc5c79fb70c7f1edb486c4723a7c680
takasing <takaty2011@gmail.com>
2016-09-14 17:13:01 +0900

    install graphviz

--- etc/init/osx/30_tools.sh
+++ etc/init/osx/30_tools.sh
@@ -4,14 +4,16 @@ trap 'echo Error: $0:$LINENO stopped; exit 1' ERR INT
 set -eu
 source "$DOTPATH"/etc/lib/vital.sh
 
-if is_exists "brew" && is_exists "easy_install" ; then
+#if is_exists "brew" && is_exists "easy_install" ; then
+if is_exists "brew"; then
   case "$PLATFORM" in
     osx)
-      brew install freetype openjpeg
-      sudo easy_install blockdiag
-      sudo easy_install seqdiag
-      sudo easy_install actdiag
-      sudo easy_install nwdiag
+      #brew install freetype openjpeg
+      #sudo easy_install blockdiag
+      #sudo easy_install seqdiag
+      #sudo easy_install actdiag
+      #sudo easy_install nwdiag
+      brew install graphviz
       ;;
     *)
       echo "???"
