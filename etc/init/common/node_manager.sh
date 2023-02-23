d87353fbeb193afbce2885f7e8a851d00f448513
t104fk <t104fk@gmail.com>
2023-02-23 17:19:48 +0900

    Avoid permission error

--- etc/init/common/node_manager.sh
+++ etc/init/common/node_manager.sh
@@ -4,6 +4,6 @@ trap 'echo Error: $0:$LINENO stopped; exit 1' ERR INT
 set -eu
 source "$DOTPATH"/etc/lib/vital.sh
 
-curl -fsSL https://raw.githubusercontent.com/tj/n/master/bin/n | bash -s lts
+curl -fsSL https://raw.githubusercontent.com/tj/n/master/bin/n | sudo bash -s lts
 
 log_pass "Success to initialize Node package manager"
