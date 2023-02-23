460147abc17dc7c728506edaec8a6568796fd595
t104fk <t104fk@gmail.com>
2023-02-23 18:20:36 +0900

    Export to use other script

--- etc/init/osx/0_brew.sh
+++ etc/init/osx/0_brew.sh
@@ -4,7 +4,7 @@ source "$DOTPATH"/etc/lib/helpers
 
 if ! is_exists "brew"; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  PATH=$PATH:/usr/local/opt/go/libexec/bin:/opt/homebrew/bin
+  export PATH=$PATH:/usr/local/opt/go/libexec/bin:/opt/homebrew/bin
   log_pass "Success to initialize Homebrew"
 else
   log_warn "Homebrew is already installed."
